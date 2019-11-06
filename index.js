ae1ad89448e5f807e6914c502df2f4aff833054c
Oleksandr <endlesskwazar@gmail.com>
2019-11-06 18:22:02 +0200

    init

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const passportJWT = require('passport-jwt');
+const swaggerUi = require('swagger-ui-express');
 
-const {strategy} = require('./auth/strategy');
-const {routes} = require('./routes');
+const { strategy } = require('./auth/strategy');
+const { routes } = require('./routes');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
@@ -14,6 +16,14 @@ app.use(passport.initialize());
 
 // parse oly json via body-parser
 app.use(bodyParser.json());
+
+var options = {
+    swaggerOptions: {
+        validatorUrl: null
+    }
+};
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
 
 app.use(routes);
 
